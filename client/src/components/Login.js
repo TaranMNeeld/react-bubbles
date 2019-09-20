40c2af0dba3c7fa77d9a3be4f18da9a2f8ce247a
Taran <thetaranneeld@gmail.com>
2019-09-20 09:23:44 -0600

    setup authentication

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,59 @@
-import React from "react";
+import React from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
-};
+class Login extends React.Component {
+    state = {
+        credentials: {
+            username: '',
+            password: ''
+        }
+    };
+
+    handleChange = event => {
+        this.setState({
+            credentials: {
+                ...this.state.credentials,
+                [event.target.name]: event.target.value
+            }
+        });
+    };
+
+    login = event => {
+        event.preventDefault();
+        axiosWithAuth().post('/login', this.state.credentials)
+            .then(res => {
+                localStorage.setItem('token', res.data.payload);
+                this.props.history.push('/protected');
+                console.log(this.state.credentials);
+            })
+            .catch(err => console.log(err))
+    };
+
+    render() {
+        return (
+            <div className='form-div'>
+                <form onSubmit={this.login}>
+                    <input
+                        className='form-field'
+                        type='text'
+                        name='username'
+                        placeholder='username'
+                        value={this.state.credentials.username}
+                        onChange={this.handleChange}
+                    />
+                    <input
+                        className='form-field'
+                        type='password'
+                        name='password'
+                        placeholder='password'
+                        value={this.state.credentials.password}
+                        onChange={this.handleChange}
+                    />
+                    <button>Log in</button>
+                </form>
+            </div>
+        );
+    }
+}
 
 export default Login;
